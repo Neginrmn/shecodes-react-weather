07b1677aa18467abcc929ee879b3bb2c9e6aaa24
Neginrmn <negin.pirmohammadi@gmail.com>
2022-05-01 17:51:02 +0430

    added footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-  weather app
+      <h1>weather app </h1>
+      <footer>
+  This project is coded by Negin{" "}<a href='https://app.netlify.com/sites/reactweatherapp4shecodes/settings/domain' target='_blank' rel="noreferrer">find it on Github</a>
+  </footer>
     </div>
   );
 }
