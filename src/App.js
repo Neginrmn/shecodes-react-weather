c33a15fafdc89aca5f8c154207006570d70ea010
Neginrmn <negin.pirmohammadi@gmail.com>
2022-05-02 18:36:58 +0430

    adde bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>weather app </h1>
+      <button className='btn btn-danger'>Hello</button>
       <footer>
   This project is coded by Negin{" "}<a href='https://app.netlify.com/sites/reactweatherapp4shecodes/settings/domain' target='_blank' rel="noreferrer">find it on Github</a>
   </footer>
