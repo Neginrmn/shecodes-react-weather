be75a3b6a2740168ea3f90f613ce797cd5c8d881
Neginrmn <negin.pirmohammadi@gmail.com>
2022-05-04 00:37:39 +0430

    humidity isn't fixed yet

--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
 
-export default function Weather() {
-  return(
-    <div className="Weather">
-       
-      <form>
-      <div className='row'>
-         <div className='col-9'>
-        <input type="search" placeholder='Find the weather for...' className='form-control' autoFocus='on' />
-        </div>
-        <div className='col-3'>
-       <input type='submit' value='search' className='btn btn-danger' />
-       </div>
-       </div>
-      </form>  
-      <h1>Shiraz</h1>
-      <ul>
-        <li>Monday 7:00</li>
-        <li>Motly cloudy</li>
-      </ul>
-      <div className='row'>
-        <div className='col-6'>
-          <img src='https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png' alt='clouds'/><span className='temp'>19</span><span className='unit'>°C</span> 
-        </div>
-        <div className='col-6'>
-          <ul>
-            <li>preciptation</li>
-            <li>humidity</li>
-            <li>wind</li>
-          </ul>
+
+export default function Weather() { 
+  const [ready,setReady] = useState(false);
+  const [weatherData, setWeatherData]= useState({});
+  const [temperature, setTemperature]= useState(null);
+  function handleResponse(res) {
+    setWeatherData({
+      temperature:res.data.main.temp,
+      wind: res.data.wind.speed,
+      city: res.data.name,
+      humidity: res.data.main.humidity,
+    })
+setTemperature();
+setReady(true);
+  }
+  if(ready) {
+    return(
+      <div className="Weather">    
+        <form>
+        <div className='row'>
+           <div className='col-9'>
+          <input type="search" placeholder='Find the weather for...' className='form-control' autoFocus='on' />
+          </div>
+          <div className='col-3'>
+         <input type='submit' value='search' className='btn btn-danger' />
+         </div>
+         </div>
+        </form>  
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Monday 7:00</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className='row'>
+          <div className='col-6'>
+            <img src='https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png' alt='clouds'/><span className='temp'>{Math.round(weatherData.temperature)}</span><span className='unit'>°C</span> 
+          </div>
+          <div className='col-6'>
+            <ul>
+              
+              <li>{weatherData.humidity} %</li>
+              <li>{weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    );
+  } else {
+    let city = 'Shiraz';
+    const APIkey = '1362dc5f45de045dab893a3af8adf6e4';
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
+    return 'Please Wait';
+  } 
 }
