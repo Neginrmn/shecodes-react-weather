537ded7f169e26317e7c464bea75dfcbb2ccd4e2
Neginrmn <negin.pirmohammadi@gmail.com>
2022-05-13 22:56:55 +0430

    forecast section is finished

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,33 +1,52 @@
-import React, { useState } from 'react';
-import './WeatherForecast.css';
-import axios from 'axios';
-import WeatherForecastDay from './WeatherForecastDay';
+import React, { useState, useEffect } from "react";
 
-export default function WeatherForcast(props) {
-  let[loaded,setLoaded]=useState(false);
-  let[forecast,setForecast]=useState(null);
-  function handleResponse(res) {
-    setForecast(res.data.daily);
+import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay.js";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
-    return(
-      <div className='WeatherForecast'>
-        <div className='row'>
-          <div className='col'>
-             <WeatherForecastDay data={forecast[0]} />
-         </div>
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
         </div>
       </div>
-      );
+    );
   } else {
-    let APIKey='1362dc5f45de045dab893a3af8adf6e4';
-     let longitude=props.coordinates.lon;
-      let latitude=props.coordinates.lat;
-      let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=metric`;
-      axios.get(apiUrl).then(handleResponse);
-       return null;
+    let APIKey='458a27d0841c1139f24bbf7e6a49edc0';
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
   }
 }
 
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,13 +1,33 @@
 import React from "react";
-import WeatherIcon from './WeatherIcon';
+import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
-<div>
-      <div className='WeatherForecast-day'>{props.data[0].dt}</div>
-           <WeatherIcon code={props.data[0].weather[0].icon} size={36} />
-            <div className='Weatherforecast-temperature'><span className='WeatherForecast-temperature-max'>{props.data[0].temp.max}°</span><span className='WeatherForecast-temperature-min'>{props.data[0].temp.min}°</span>
+    <div>
+      <div className="ForecastDay">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="ForecastTemperatures">
+        <span className="ForecastTemperatureMax">{maxTemperature()}</span>
+        <span className="ForecastTemperatureMin">{minTemperature()}</span>
       </div>
-</div>
-);
+    </div>
+  );
 }
