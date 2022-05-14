2b8b304cf7edd5dc4486c4e96b2f4df7e96a1304
Neginrmn <negin.pirmohammadi@gmail.com>
2022-05-14 17:14:11 +0430

    fixed the temperature position

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ export default function App() {
     <div className="App">
       <Weather defaultCity="Paris"/>
       <footer>
-  This project is coded by Negin{" "}<a href='https://app.netlify.com/sites/reactweatherapp4shecodes/settings/domain' target='_blank' rel="noreferrer">find it on Github</a>
+  This project is coded by Negin.{" "}Find it on{" "}<a href='https://github.com/Neginrmn/shecodes-react-weather' target='_blank' rel="noreferrer">Github</a>
   </footer>
     </div>
   );
 }
 //colors #347e8f - #0f5069 - #09394c - #00364b
-
+// https://app.netlify.com/sites/reactweatherapp4shecodes/settings/domain
